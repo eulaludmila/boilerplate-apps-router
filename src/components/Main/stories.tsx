c3a5a865a4af9bdb8f14b20e953f73a15a3ce154
Eula Ludmila <eula.silva.teixeira@gmail.com>
2024-07-23 22:00:02 -0300

    feat(storybook): creating controls

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -4,9 +4,20 @@ import Main from '.'
 export default {
   title: 'Main',
   component: Main,
+  // args: {
+  //   title: 'Default',
+  //   description: 'description default'
+  // },
   parameters: {
     layout: 'fullscreen'
   }
 } as Meta //funciona para configurar um story de um componente
 
 export const Default: StoryObj = {}
+
+export const Basic: StoryObj = {
+  args: {
+    title: 'Basic',
+    description: 'description basic'
+  }
+}
